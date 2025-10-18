a5c23c689aab3ff8b5b0cfd91baa943abda5687d
radinka alifasya <radinka.alifasya@gmail.com>
2025-10-18 14:41:15 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,11 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   reactStrictMode: false,
   images: {
-    unoptimized: true
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'unpkg.com',
+      },
+    ],
   },
-  trailingSlash: true
 }
 
 module.exports = nextConfig
